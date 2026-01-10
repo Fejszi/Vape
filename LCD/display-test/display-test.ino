0a601df634f481ed50fa81504353b8f37be95b8e
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2026-01-10 17:06:43 +0100

    Added automatic display size test

---
+++ LCD/display-test/display-test.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#define TFT_CS   4
+#define TFT_RST  5 
+#define TFT_DC   6
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+uint16_t width = 100;
+uint16_t height = 100;
+char buffer[100];
+
+void setup(void) {
+  Serial.begin(115200);
+  tft.setSPISpeed(400000);
+  tft.invertDisplay(false);
+  tft.setRotation(1);
+}
+
+void loop(){
+  tft.init(width, height);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(1);
+  tft.setTextWrap(true);
+  sprintf(buffer, "%dx%d\n");
+  tft.print(buffer);
+  delay(500);
+
+  // Uncomment for width/height test
+  //width++;
+  //height++;
+}
+
